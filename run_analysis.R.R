19e51bfbb7e1b3a1982f2738c5a23d8dbc2db242
Davon <davon.dicks@gmail.com>
2019-02-28 18:39:02 -0500

    Update run_analysis.R.R

--- run_analysis.R.R
+++ run_analysis.R.R
@@ -1,4 +1,4 @@
-## Reading all the files I need into R
+## Reading all the tables into R
 Feature <- read.table(file.choose())
 activityLabels <- read.table(file.choose())
 x_train <- read.table(file.choose())
@@ -19,24 +19,20 @@ colnames(activityLabels) <- c("activityId", "activityType")
 M_train <- cbind(y_train, subject_train, x_train)
 M_Test <- cbind(y_test,subject_test,x_test)
 Prime <- rbind(M_train, M_Test)
-## Creating a function that will search through the "prime" file
-## and find the the mean and standard deviation included both ID's
+## Creating a variable that will search through the "prime" file and find the the mean and standard deviation included both ID's
 Prime_Names <- colnames(Prime)
 mean_and_std <- (grepl("activityId" , Prime_Names)
                  grepl("subjectId" , Prime_Names)
                  grepl("mean.." , Prime_Names) 
                  grepl("std.." , Prime_Names) )
-##Now we need to take this variable and put it in a subset that
-## includes all the TRUE values
+##Creating a  subset that includes all the TRUE values
 Prime_meanandstd <- Prime[,mean_and_std == TRUE]
-## Now we want to add the activity name to the new dataset by merging
-## it with the activity label using the activityId
+## Adding the activity name to the new dataset by merging it with the activity label using the activityId
 Prime_ActNames <- merge(Prime_meanandstd, activityLabels, 
                         by = "activityId", all.x = TRUE )
-## Creating a second, independent tidy data set with the average
-##of each variable for each activity and each subject
+## Creating a second, independent tidy data set with the average of each variable for each activity and each subject
 Prime_clean <- aggregate(. ~subjectId + activityId, Prime_ActNames, mean)
-## Order the data first by subjectId then ActivityId
+## Ordering the data first by subjectId then ActivityId
 Prime_clean <- Prime_clean[order(Prime_clean$subjectId, Prime_clean$activityId),]
-# upload your data set as a txt file created with write.table()
+# uploading dataset as a txt file created with write.table()
 write.table(Prime_clean, "Prime_clean.txt", row.names = FALSE)
